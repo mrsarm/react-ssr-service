a948c4c794eb262c0d1fc9a77b026d8426d1c828
Mariano Ruiz <mrsarm@gmail.com>
2024-04-06 13:37:29 -0300

    Fix test output

--- src/components/Sum.test.tsx
+++ src/components/Sum.test.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Sum from './Sum';
 
-test('renders sum of 2 numbers', () => {
-  render(<Sum num1={10} num2={2} />);
-  const numbers = screen.getByText(/Numbers → \(10, 2\)/);
-  expect(numbers).toBeInTheDocument();
-  const sum = screen.getByText(/10 \+ 2 = 12/);
-  expect(sum).toBeInTheDocument();
-});
+describe('Sum', () => {
+  test('renders sum of 2 numbers', () => {
+    render(<Sum num1={10} num2={2} />);
+    const numbers = screen.getByText(/Numbers → \(10, 2\)/);
+    expect(numbers).toBeInTheDocument();
+    const sum = screen.getByText(/10 \+ 2 = 12/);
+    expect(sum).toBeInTheDocument();
+  });
 
-test('throw error with unsafe sum', () => {
-  expect(() => render(<Sum num1={Number.MAX_SAFE_INTEGER} num2={1} />))
-    .toThrow(`Sum of ${Number.MAX_SAFE_INTEGER} and 1 is not safe.`);
+  test('throw error with unsafe sum', () => {
+    const expectedError = `Sum of ${Number.MAX_SAFE_INTEGER} and 1 is not safe.`;
+    const oldError = console.error;
+    console.error = (msg: string) => {
+      if (!msg.includes(expectedError) && !msg.includes('Consider adding an error boundary')) {
+        oldError(msg);
+      }
+    }
+    expect(() => render(<Sum num1={Number.MAX_SAFE_INTEGER} num2={1} />))
+      .toThrow(expectedError);
+    console.error = oldError;
+  });
 });
